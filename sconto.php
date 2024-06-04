26e36f7acba51dbad4102c73c16d0171e54b3ed5
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-04 15:51:21 +0200

    add: creation of a personal exception

--- sconto.php
+++ sconto.php
@@ -4,6 +4,12 @@ trait Sconto {
     // sconto impostato su 0 per iniziare
     private $sconto = 0;
 
+    public function setSconto($sconto){
+        if($sconto < 0 || $sconto > 100){
+            throw new InvalidScontoException("Lo sconto deve essere compreso tra 0% e 100%");
+        }
+    }
+
 }
 
 ?>
